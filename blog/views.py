9094b9d4c08e696334ab62359521d56e5e27c3aa
user <no@an.email>
2021-05-25 12:04:14 +0800

    下载一个博客模板,然后修改它使得它的HTML能够作为django的模板,显示存在于本地数据库中的posts

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 from django.shortcuts import render
 
-# Create your views here.
-# urls.py刚刚添加了对应着index的路径:''(即根目录)
+
+# 所有post的列表
 def index(req):
-    # context传入index.html作为其中{{}}包含的元素的替代
-    return render(req, 'blog/index.html', context={
-        'title': 'Ge\'s blog main page',
-        'welcome': 'Welcome to my main page'
-    })
-    return HttpResponse('hello, this is Ge\'s blog')
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(req, 'blog/index.html', context={'post_list': post_list})
+# urls.py刚刚添加了对应着index的路径:''(即根目录)
+# def index(req):
+#     # context传入index.html作为其中{{}}包含的元素的替代
+#     return render(req, 'blog/index.html', context={
+#         'title': 'Ge\'s blog main page',
+#         'welcome': 'Welcome to my main page'
+#     })
+#     return HttpResponse('hello, this is Ge\'s blog')
