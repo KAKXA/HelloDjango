606ef56970682ff728213b432bdc63f4c8623f7c
user <no@an.email>
2021-05-30 21:52:31 +0800

    在文中加入目录

--- blog/views.py
+++ blog/views.py
@@ -17,14 +17,28 @@ def index(req):
 #     })
 #     return HttpResponse('hello, this is Ge\'s blog')
 def detail(req, pk):
-    # 如果object存在就返回object否则返回404
     post = get_object_or_404(Post, pk=pk)
-    # render 是将给定的模板和上下文和上下文字典组合
     post.body = markdown.markdown(post.body,
     extensions=[
         'markdown.extensions.extra',
         'markdown.extensions.codehilite',
         'markdown.extensions.toc'
     ])
-    # 此时post.body是经过markdown解析过后的HTML文本
     return render(req, 'blog/detail.html', context={'post': post})
+
+def detail(request, pk):
+    # 如果object存在就返回object否则返回404
+    post = get_object_or_404(Post, pk=pk)
+    md = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        'markdown.extensions.toc',
+    ])
+    # 用convert方法将post.body中地Markdown文本解析成HTML文本
+    post.body = md.convert(post.body)
+    # 在经过上面这行以后,md拥有了toc属性
+    # 为post实例动态地添加了toc属性
+    post.toc = md.toc
+    # render 是将给定的模板和上下文和上下文字典组合
+    # 此时post.body是经过markdown解析过后的HTML文本
+    return render(request, 'blog/detail.html', context={'post': post})
